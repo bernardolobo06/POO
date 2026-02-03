13d461f31f07a207bd2fd187695b209b2d117459
Marcos Bernardo da Silva Lobo <marcosbernardolobo2006@gmail.com>
2026-02-03 00:43:20 +0000

    Add Main and Maximo classes for maximum value calculation

---
+++ src/ficha1/maximo/Main.java
@@ -0,0 +1,24 @@
+package ficha1.maximo;
+
+import java.util.Scanner;
+
+public class Main {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Maximo max = new Maximo();
+
+        System.out.println("Primeiro Operando:");
+        int n1 = sc.nextInt();
+
+        System.out.println("Segundo Operando:");
+        int n2 = sc.nextInt();
+
+        System.out.println("Máximo: " + max.maximo(n1, n2));
+
+        sc.close();
+    }
+    
+}
---
+++ src/ficha1/maximo/Maximo.java
@@ -0,0 +1,12 @@
+package ficha1.maximo;
+
+public class Maximo {
+
+    public int maximo(int a, int b) {
+        
+        if (a > b) return a;
+        else return b;
+
+    }
+
+}
