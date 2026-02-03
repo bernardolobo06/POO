5c4fd85f14f87a60e4cdff4e2ce5d5a251c954da
Marcos Bernardo da Silva Lobo <marcosbernardolobo2006@gmail.com>
2026-02-03 01:07:00 +0000

    Add Conversao and Main classes for currency conversion functionality

---
+++ src/ficha1/conversao/Conversao.java
@@ -0,0 +1,9 @@
+package ficha1.conversao;
+
+public class Conversao {
+
+    public double eurosParaLibras(double valor, double taxaConversao) {
+        return valor * taxaConversao;
+    }
+
+}
---
+++ src/ficha1/conversao/Main.java
@@ -0,0 +1,24 @@
+package ficha1.conversao;
+
+import java.util.Scanner;
+
+public class Main {
+
+    @SuppressWarnings("ConvertToTryWithResources")
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Conversao c = new Conversao();
+
+        System.out.println("Valor a converter:");
+        double valor = sc.nextDouble();
+
+        System.out.println("Taxa de conversão:");
+        double taxa = sc.nextDouble();
+
+        System.out.println("Valor: " + c.eurosParaLibras(valor, taxa));
+
+        sc.close();
+    }
+    
+}
